ecc5a5738490feae1ec95074cf6469a1b8a3f4d3
zhaobo <1394654517@qq.com>
2018-05-07 17:11:13 +0800

    加了BaseController，并将request,response,session,application封装在内，其余controller只需继承即可

--- src/main/java/com/zhaobo/cruddemo/controller/BaseController.java
+++ src/main/java/com/zhaobo/cruddemo/controller/BaseController.java
@@ -13,6 +13,12 @@ public class BaseController {
     HttpServletResponse response;
     HttpSession session;
     ServletContext application;
+/**
+ *  页面请求只要进入这个控制器，不管请求那个方法，均会先执行被@ModelAttribute标记的方法，
+ 所以我们可以用@ModelAttribute注解的方法做一些初始化操作。当同一个controller中有多
+ 个方法被@ModelAttribute注解标记，所有被@ModelAttribute标记的方法均会被执行，按先
+ 后顺序执行，然后再进入请求的方法。
+ */
 
     @ModelAttribute
     private void set(HttpServletRequest request,HttpServletResponse response){
